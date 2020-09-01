8c9675f3b20e0d5a8bd9cb15a599d6d2418392fc
Katerina964 <katrin.balakina@gmail.com>
2020-09-01 14:59:19 +0300

    Backup

---
+++ test_django/article/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+class ArticleSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=120)
+    description = serializers.CharField()
+    body = serializers.CharField()
+    author_id = serializers.IntegerField()
+    def create(self, validated_data):
+        return Article.objects.create(**validated_data)
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.description = validated_data.get('description', instance.description)
+        instance.body = validated_data.get('body', instance.body)
+        instance.author_id = validated_data.get('author_id', instance.author_id)
+        instance.save()
+        return instance
+    def delete(self, request, pk):
+    # Get object with this pk
+        article = get_object_or_404(Article.objects.all(), pk=pk)
+        article.delete()
+        return Response({
+            "message": "Article with id `{}` has been deleted.".format(pk)
+        }, status=204)
